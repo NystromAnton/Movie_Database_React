d8b39a047216b8431a16ab5ffcc9a43c2a4145e0
Anton Nyström <anton1nystrom@gmail.com>
2021-05-11 16:23:00 +0200

    Updated navbar

--- frontend/rockcorn/src/components/Navbar.js
+++ frontend/rockcorn/src/components/Navbar.js
@@ -8,20 +8,14 @@ class Topbar extends React.Component {
     render () {
         return (
             <Navbar bg="#FFFFFF" expand="lg">
-            <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="/">Rockcorn</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
+                <Nav.Link href="/">Help</Nav.Link>
                 <Nav.Link href="/">About us</Nav.Link>
-                <Nav.Link href="/">Contact</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-            </Nav>        
+                <Nav.Link href="/">Contact</Nav.Link>                
+            </Nav>
             </Navbar.Collapse>
             </Navbar>
         )
